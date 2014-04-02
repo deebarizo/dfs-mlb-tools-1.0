3f50b04b45b5dbbd720b1fb57e3ddb0c58b56c37
deebarizo <deebarizo@gmail.com>
2014-04-02 18:35:55 -0500

    Created three column fixed layout

--- application/views/daily_fd.php
+++ application/views/daily_fd.php
@@ -1,37 +1,113 @@
-		<div id="wrapper">
+		<div class="col-wrapper">
 			<section>
-	      		<h3><i class="fa fa-upload"></i> <?php echo $subhead; ?></h3>
-	  
-	      		<?php if (isset($error)) { ?>
-			      			<p style="color:red"><?php echo $error; ?></p>
-	      		<?php } ?>
-	
-				<?php if (isset($fstats_fd)) { ?>
-					<table>
-						<thead>
-							<tr>
-								<th data-sort='string'>Pos</th>
-								<th data-sort='string'>Name</th>
-								<th data-sort='float'>FPPG</th>
-								<th data-sort='int'>Played</th>
-								<th data-sort='string'>Team</th>
-								<th data-sort='string'>Opp</th>
-								<th data-sort='string'>Salary</th>
-							</tr>
-						</thead>
-						<tbody>
-							<?php foreach ($fstats_fd as $key => $value) { ?>
-	 							<tr>
-									<td><?php echo $value['position']; ?></td>
-									<td><?php echo $value['name']; ?></td>
-									<td><?php echo $value['fppg']; ?></td>
-									<td><?php echo $value['num_games']; ?></td>
-									<td><?php echo $value['team']; ?></td>
-									<td><?php echo $value['opponent']; ?></td>
-									<td><?php echo $value['salary']; ?></td>
+				<div class="col col-left">
+		      		<h3><?php echo $subhead; ?></h3>
+		  
+		      		<?php if (isset($error)) { ?>
+				      			<p style="color:red"><?php echo $error; ?></p>
+		      		<?php } ?>
+		
+					<?php if (isset($fstats_fd)) { ?>
+						<table>
+							<thead>
+								<tr>
+									<th data-sort='string'>Pos</th>
+									<th data-sort='string'>Name</th>
+									<th data-sort='float'>FPPG</th>
+									<th data-sort='int'>G</th>
+									<th data-sort='string'>Team</th>
+									<th data-sort='string'>Opp</th>
+									<th data-sort='string'>Salary</th>
 								</tr>
-							<?php } ?>
-						</tbody>
-					</table>
-				<?php } ?>
+							</thead>
+							<tbody>
+								<?php foreach ($fstats_fd as $key => $value) { ?>
+		 							<tr>
+										<td><?php echo $value['position']; ?></td>
+										<td><?php echo $value['name']; ?></td>
+										<td><?php echo $value['fppg']; ?></td>
+										<td><?php echo $value['num_games']; ?></td>
+										<td><?php echo $value['team']; ?></td>
+										<td><?php echo $value['opponent']; ?></td>
+										<td><?php echo $value['salary']; ?></td>
+									</tr>
+								<?php } ?>
+							</tbody>
+						</table>
+					<?php } ?>
+				</div>
+
+				<div class="col col-mid">
+		      		<h3><?php echo $subhead; ?></h3>
+		  
+		      		<?php if (isset($error)) { ?>
+				      			<p style="color:red"><?php echo $error; ?></p>
+		      		<?php } ?>
+		
+					<?php if (isset($fstats_fd)) { ?>
+						<table>
+							<thead>
+								<tr>
+									<th data-sort='string'>Pos</th>
+									<th data-sort='string'>Name</th>
+									<th data-sort='float'>FPPG</th>
+									<th data-sort='int'>G</th>
+									<th data-sort='string'>Team</th>
+									<th data-sort='string'>Opp</th>
+									<th data-sort='string'>Salary</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php foreach ($fstats_fd as $key => $value) { ?>
+		 							<tr>
+										<td><?php echo $value['position']; ?></td>
+										<td><?php echo $value['name']; ?></td>
+										<td><?php echo $value['fppg']; ?></td>
+										<td><?php echo $value['num_games']; ?></td>
+										<td><?php echo $value['team']; ?></td>
+										<td><?php echo $value['opponent']; ?></td>
+										<td><?php echo $value['salary']; ?></td>
+									</tr>
+								<?php } ?>
+							</tbody>
+						</table>
+					<?php } ?>
+				</div>
+
+				<div class="col col-right">
+		      		<h3><?php echo $subhead; ?></h3>
+		  
+		      		<?php if (isset($error)) { ?>
+				      			<p style="color:red"><?php echo $error; ?></p>
+		      		<?php } ?>
+		
+					<?php if (isset($fstats_fd)) { ?>
+						<table>
+							<thead>
+								<tr>
+									<th data-sort='string'>Pos</th>
+									<th data-sort='string'>Name</th>
+									<th data-sort='float'>FPPG</th>
+									<th data-sort='int'>G</th>
+									<th data-sort='string'>Team</th>
+									<th data-sort='string'>Opp</th>
+									<th data-sort='string'>Salary</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php foreach ($fstats_fd as $key => $value) { ?>
+		 							<tr>
+										<td><?php echo $value['position']; ?></td>
+										<td><?php echo $value['name']; ?></td>
+										<td><?php echo $value['fppg']; ?></td>
+										<td><?php echo $value['num_games']; ?></td>
+										<td><?php echo $value['team']; ?></td>
+										<td><?php echo $value['opponent']; ?></td>
+										<td><?php echo $value['salary']; ?></td>
+									</tr>
+								<?php } ?>
+							</tbody>
+						</table>
+					<?php } ?>
+				</div>
 			</section>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,7 +2,11 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		
 		<title><?php echo $page_title; ?></title>
+
+		<?php echo link_tag('css/normalize.css'); ?>
+		<?php echo link_tag('css/main.css'); ?>
 	</head>
 	<body>
 		<header>
