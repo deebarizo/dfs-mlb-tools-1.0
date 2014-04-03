ba7269ba8f37e556c3f10a0beef8fe2344776af9
deebarizo <deebarizo@gmail.com>
2014-04-02 23:22:07 -0500

    Worked on the optimal lineup feature

--- application/views/update_fd.php
+++ application/views/update_fd.php
@@ -1,22 +1,24 @@
 		<div id="wrapper">
 			<section>
-	      		<h3><i class="fa fa-upload"></i> <?php echo $subhead; ?></h3>
+				<div class="col">
+		      		<h3><i class="fa fa-upload"></i> <?php echo $subhead; ?></h3>
 
-	      		<?php if ($message != 'Form validation error.') { ?>
-			      			<p style="color:red"><?php echo $message; ?></p>
-	      		<?php } ?>
+		      		<?php if ($message != 'Form validation error.') { ?>
+				      			<p style="color:red"><?php echo $message; ?></p>
+		      		<?php } ?>
 
-				<?php echo form_open(base_url().'update/fd'); ?>
-					<p>
-				        <label for="url"><h4>FD Salaries URL</h4></label> 
-				        <input style='width: 650px' id="url" type="url" name="url" />
-				        <?php echo form_error('url'); ?>
-					</p>
+					<?php echo form_open(base_url().'update/fd'); ?>
+						<p>
+					        <label for="url"><h4>FD Salaries URL</h4></label> 
+					        <input style='width: 650px' id="url" type="url" name="url" />
+					        <?php echo form_error('url'); ?>
+						</p>
 
-					<p>
-				        <br>
-				        <?php echo form_submit( 'submit', 'Submit'); ?>
-					</p>
+						<p>
+					        <br>
+					        <?php echo form_submit( 'submit', 'Submit'); ?>
+						</p>
 
-				<?php echo form_close(); ?>
+					<?php echo form_close(); ?>
+				</div>
 			</section>
