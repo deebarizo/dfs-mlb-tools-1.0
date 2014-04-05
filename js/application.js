e949d5759135a332315dfa9fcdc74399c22483a0
deebarizo <deebarizo@gmail.com>
2014-04-04 23:38:43 -0500

    Worked on optimal lineup solver

--- js/application.js
+++ js/application.js
@@ -147,8 +147,6 @@ $(document).ready(function() {
 						  sortedSalaries["OF"][1][4] + 
 						  sortedSalaries["OF"][2][4];
 
-		console.log(totalSalary);
-
 		var diff = totalSalary - 35000;
 
 		if (totalSalary > 35000) {
@@ -157,7 +155,6 @@ $(document).ready(function() {
 		}
 
 		var positionCount = 0;
-		var rankCount = 0;
 		var changeTracker = [
 			["P", 0], 
 			["C", 0], 
@@ -170,57 +167,40 @@ $(document).ready(function() {
 			["OF", 2]
 		];
 
-		if (diff * -1 > 300) {
+		if (diff * -1 > 300) { 
 			for (var i = 0; i < 500; i++) {
-				if (typeof sortedSalaries[changeTracker[positionCount][0]][changeTracker[rankCount][1]] == "undefined") {
-					if (positionCount == 6) {
-						positionCount = 0;
-					} else {
-						positionCount++;
-					}
-					
-					continue;
+				if (positionCount >= 6) {
+					changeTracker[positionCount][1] += 3;
 				} else {
-					if (positionCount <= 5) {
-						changeTracker[positionCount][1]++;
-					} else {
-						changeTracker[positionCount][1] += 3;
-					}
-
-					totalSalary = sortedSalaries["P"][changeTracker[0][1]][4] + 
-								  sortedSalaries["C"][changeTracker[1][1]][4] + 
-								  sortedSalaries["1B"][changeTracker[2][1]][4] + 
-								  sortedSalaries["2B"][changeTracker[3][1]][4] + 
-								  sortedSalaries["3B"][changeTracker[4][1]][4] + 
-								  sortedSalaries["SS"][changeTracker[5][1]][4] + 
-								  sortedSalaries["OF"][changeTracker[6][1]][4] + 
-								  sortedSalaries["OF"][changeTracker[7][1]][4] + 
-								  sortedSalaries["OF"][changeTracker[8][1]][4];
-
-					diff = totalSalary - 35000;
-
-					console.log(totalSalary);
-
-					if (totalSalary > 35000) {
-						if (positionCount <= 5) {
-							changeTracker[positionCount][1]--;
-						} else {
-							changeTracker[positionCount][1] -= 3;
-						}
-
-						if (positionCount == 6) {
-							positionCount = 0;
-						} else {
-							positionCount++;
-						}					
-						
-						continue;
-					} else if (diff * -1 > 300) {
-						continue;
-					} else {
-						alert("Here is the optimal lineup total salary $"+totalSalary+".");
-					}
+					changeTracker[positionCount][1]++;
 				}
+
+				totalSalary = sortedSalaries["P"][changeTracker[0][1]][4] + 
+							  sortedSalaries["C"][changeTracker[1][1]][4] + 
+							  sortedSalaries["1B"][changeTracker[2][1]][4] + 
+							  sortedSalaries["2B"][changeTracker[3][1]][4] + 
+							  sortedSalaries["3B"][changeTracker[4][1]][4] + 
+							  sortedSalaries["SS"][changeTracker[5][1]][4] + 
+							  sortedSalaries["OF"][changeTracker[6][1]][4] + 
+							  sortedSalaries["OF"][changeTracker[7][1]][4] + 
+							  sortedSalaries["OF"][changeTracker[8][1]][4];
+
+				diff = totalSalary - 35000;
+
+				console.log(totalSalary);
+
+				if (totalSalary > 35000) {
+					if (positionCount <= 5) {
+						changeTracker[positionCount][1]--;
+					} else {
+						changeTracker[positionCount][1] -= 3;
+					}
+
+					positionCount++;	
+				}	
+
+				console.log("changeTracker: "+changeTracker);
+				console.log("positionCount: "+positionCount);
 			}		
 		} else {
 			alert("Here is the optimal lineup total salary $"+totalSalary+".");
