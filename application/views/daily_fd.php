ac4821864f5be13ee060c8beafbd9c817181a7dd
deebarizo <deebarizo@gmail.com>
2014-04-07 19:13:46 -0500

    Cleaned up links on daily fd view

--- application/views/daily_fd.php
+++ application/views/daily_fd.php
@@ -6,15 +6,13 @@
 					<table class="salaries">
 						<thead>
 							<tr>
-								<th data-sort='string'>Date</th>
-								<th data-sort='string'>Time</th>
+								<th data-sort='string'>Date (Time)</th>
 							</tr>
 						</thead>
 						<tbody>
 							<?php foreach ($league as $key => $value) { ?>
 	 							<tr>
-									<td><a href="<?php echo base_url().'daily/fd/'.$value['date'].'/'.$value['time']; ?>"><?php echo $value['date']; ?></a></td>
-									<td><a href="<?php echo base_url().'daily/fd/'.$value['date'].'/'.$value['time']; ?>"><?php echo $value['capitalized_time']; ?></a></td>
+									<td><a href="<?php echo base_url().'daily/fd/'.$value['date'].'/'.$value['time']; ?>"><?php echo $value['date']; ?> (<?php echo $value['capitalized_time']; ?>)</a></td>
 								</tr>
 							<?php } ?>
 						</tbody>
